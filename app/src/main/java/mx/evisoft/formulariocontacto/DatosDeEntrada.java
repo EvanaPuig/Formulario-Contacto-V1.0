888cfa5a90910ab4c7081eef77c57a6d5ec7c969
emp233 <emp233@msn.com>
2016-07-14 13:11:13 -0500

    Initial commit

---
+++ app/src/main/java/mx/evisoft/formulariocontacto/DatosDeEntrada.java
@@ -0,0 +1,13 @@
+package mx.evisoft.formulariocontacto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DatosDeEntrada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.datos_de_entrada);
+    }
+}
